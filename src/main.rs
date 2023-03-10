9439af610a7747154eb780888f8e25c4d8af2e9b
Sabry Awad <dr.sabry1997@gmail.com>
2023-03-11 01:25:31 +0200

    feat: Add printing the transcription to the console and the timestamps are displayed in a formatted table.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::{fs::File, io::BufReader};
+
 use base64::{engine::general_purpose, Engine};
 use bytemuck::cast_slice;
-use hound::WavReader;
+use prettytable::{format, Cell, Row, Table};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
@@ -9,6 +11,7 @@ struct RecognitionConfig {
     encoding: String,
     sample_rate_hertz: i32,
     language_code: String,
+    enable_word_time_offsets: bool,
 }
 
 #[derive(Serialize)]
@@ -23,18 +26,34 @@ struct RecognizeRequest {
 }
 
 #[derive(Deserialize)]
-struct SpeechRecognitionAlternative {
-    transcript: String,
+#[serde(rename_all = "camelCase")]
+pub struct Word {
+    end_time: String,
+    start_time: String,
+    word: String,
 }
 
 #[derive(Deserialize)]
+struct SpeechRecognitionAlternative {
+    confidence: f64,
+    transcript: String,
+    words: Vec<Word>,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 struct SpeechRecognitionResult {
     alternatives: Vec<SpeechRecognitionAlternative>,
+    language_code: String,
+    result_end_time: String,
 }
 
 #[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 struct SpeechRecognitionResponse {
+    request_id: String,
     results: Vec<SpeechRecognitionResult>,
+    pub total_billed_time: String,
 }
 
 #[tokio::main]
@@ -44,12 +63,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let audio_file_path = "sample.wav";
 
     // Read audio file content
-    let mut audio_file = WavReader::open(audio_file_path)?;
-    let sample_rate = audio_file.spec().sample_rate;
-    let mut audio_content = Vec::new();
-    for sample in audio_file.samples::<i16>() {
-        audio_content.push(sample?);
-    }
+    let audio_file = File::open(audio_file_path)?;
+    let audio_reader = BufReader::new(audio_file);
+    let audio_decoder = hound::WavReader::new(audio_reader)?;
+    let sample_rate = audio_decoder.spec().sample_rate;
+    let audio_content: Vec<i16> = audio_decoder.into_samples().collect::<Result<_, _>>()?;
+
     let audio_slice = cast_slice(&audio_content);
 
     // Encode audio content to base64
@@ -60,6 +79,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         encoding: "LINEAR16".to_string(),
         sample_rate_hertz: sample_rate as i32,
         language_code: language.to_string(),
+        enable_word_time_offsets: true,
     };
 
     let recognition_audio = RecognitionAudio {
@@ -81,14 +101,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Parse recognition response
     let recognition_response: SpeechRecognitionResponse = response.json().await?;
-    // Extract the transcription from the response and print it to the console
-    let transcription = recognition_response
-        .results
-        .iter()
-        .map(|result| result.alternatives[0].transcript.clone())
-        .collect::<Vec<String>>()
-        .join("\n");
 
-    println!("Transcription: {}", transcription);
+    let mut table = Table::new();
+    table.add_row(Row::new(vec![
+        Cell::new("Word"),
+        Cell::new("Start Time (s)"),
+        Cell::new("End Time (s)"),
+    ]));
+
+    table.set_format(*format::consts::FORMAT_BOX_CHARS);
+
+    // Extract the transcription from the response and print it to the console
+    for result in recognition_response.results {
+        for alternative in result.alternatives {
+            println!("Transcript: {}", alternative.transcript);
+            for item in alternative.words {
+                let seconds = item.start_time.trim_end_matches('s').parse::<f64>()?;
+                let start_time = format!("{:.3}", seconds);
+                let seconds = item.end_time.trim_end_matches('s').parse::<f64>()?;
+                let end_time = format!("{:.3}", seconds);
+                table.add_row(Row::new(vec![
+                    Cell::new(&item.word),
+                    Cell::new(&start_time),
+                    Cell::new(&end_time),
+                ]));
+            }
+        }
+    }
+    table.printstd();
+
     Ok(())
 }
