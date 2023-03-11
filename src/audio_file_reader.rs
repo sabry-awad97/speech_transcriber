a83c1e9437dc168d969dcbffb084cd20e104628e
Sabry Awad <dr.sabry1997@gmail.com>
2023-03-11 14:23:05 +0200

    feat: Add stream the audio file

--- src/audio_file_reader.rs
+++ src/audio_file_reader.rs
@@ -1,20 +1,73 @@
-use std::{error::Error, fs::File, io::BufReader};
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::result::Result;
 
-pub struct AudioFileReader<'a> {
-    file_path: &'a str,
+use hound;
+
+/// An iterator that yields audio data from a WAV file in chunks of a specified size.
+pub struct AudioStream {
+    pub sample_rate: u32,
+    pub duration: u32,
+    pub num_channels: usize,
+    pub buffer_size: usize,
+    pub buffer: Vec<i16>,
+    pub audio_decoder: hound::WavReader<BufReader<File>>,
 }
 
-impl<'a> AudioFileReader<'a> {
-    pub fn new(file_path: &'a str) -> Self {
-        Self { file_path }
-    }
-
-    pub fn read_audio(&self) -> Result<(Vec<i16>, u32), Box<dyn Error>> {
-        let audio_file = File::open(self.file_path)?;
+impl AudioStream {
+    /// Creates a new `AudioStream` for the given file path and buffer size.
+    pub fn new(file_path: &str, buffer_size: usize) -> Result<Self, Box<dyn Error>> {
+        let audio_file = File::open(file_path)?;
         let audio_reader = BufReader::new(audio_file);
         let audio_decoder = hound::WavReader::new(audio_reader)?;
-        let sample_rate = audio_decoder.spec().sample_rate;
-        let audio_content: Vec<i16> = audio_decoder.into_samples().collect::<Result<_, _>>()?;
-        Ok((audio_content, sample_rate))
+        let sample_rate = audio_decoder.spec().sample_rate as u32;
+        let num_channels = audio_decoder.spec().channels as usize;
+        let buffer_size_bytes = buffer_size * num_channels * 2;
+
+        Ok(Self {
+            sample_rate,
+            duration: audio_decoder.duration() / sample_rate,
+            num_channels,
+            buffer_size,
+            buffer: vec![0; buffer_size_bytes],
+            audio_decoder,
+        })
+    }
+
+    pub fn audio_content(self) -> Vec<i16> {
+        self.into_iter().collect::<Vec<Vec<i16>>>().concat()
+    }
+}
+
+impl Iterator for AudioStream {
+    type Item = Vec<i16>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let num_samples = self.buffer_size * self.num_channels;
+        let mut samples = self.audio_decoder.samples::<i16>();
+
+        let mut i = 0;
+        while let Some(sample) = samples.next() {
+            self.buffer[i] = sample.unwrap();
+            i += 1;
+            if i == num_samples {
+                break;
+            }
+        }
+
+        if i == 0 {
+            return None;
+        }
+
+        let num_samples_read = i;
+        let num_samples_needed = num_samples - num_samples_read;
+
+        if num_samples_needed > 0 {
+            self.buffer[num_samples_read..num_samples].fill(0);
+        }
+
+        let audio_samples = self.buffer[..num_samples_read].to_vec();
+        Some(audio_samples)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use model::SpeechRecognitionResponse;
 use speech_client::SpeechClient;
 use transcription_printer::{OutputFormat, TranscriptionPrinter};
 
+use crate::audio_file_reader::AudioStream;
+
 mod audio_file_reader;
 mod model;
 mod speech_client;
@@ -23,12 +25,12 @@ impl SpeechTranscriber {
     pub async fn transcribe(
         self,
         audio_content: &[i16],
-        sample_rate: u32,
+        sample_rate: &u32,
     ) -> Result<SpeechRecognitionResponse, Box<dyn Error>> {
         let audio_slice = cast_slice(&audio_content);
         let recognition_response = self
             .speech_client
-            .recognize(audio_slice, sample_rate)
+            .recognize(audio_slice, *sample_rate)
             .await?;
 
         Ok(recognition_response)
@@ -40,15 +42,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let language_code = "en-US";
     let audio_file_path = "sample.wav";
 
-    let audio_reader = audio_file_reader::AudioFileReader::new(audio_file_path);
-    let (audio_content, sample_rate) = audio_reader.read_audio()?;
+    let audio_stream = AudioStream::new(audio_file_path, 1024).unwrap();
+    let sample_rate = audio_stream.sample_rate;
 
+    let audio_content = audio_stream.audio_content();
     let speech_transcriber = SpeechTranscriber::new(language_code);
     let recognition_response = speech_transcriber
-        .transcribe(&audio_content, sample_rate)
+        .transcribe(&audio_content, &sample_rate)
         .await?;
 
-    let mut printer = TranscriptionPrinter::new(OutputFormat::Json);
+    let mut printer = TranscriptionPrinter::new(OutputFormat::Table);
     printer.print(&recognition_response, None);
 
     Ok(())
